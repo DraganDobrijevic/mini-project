5ff89b6092df8193882b4ef78b595cb86296db34
dragandobrijevic <dragan16m@gmail.com>
2021-03-05 19:45:47 +0100

    task3-openlayers

--- src/pages/zadatak3/zadatak3.component.jsx
+++ src/pages/zadatak3/zadatak3.component.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import { useEffect } from 'react';
+import 'ol/ol.css';
+import './zadatak3.styles.css';
+import { Map, View } from 'ol';
+// import TileLayer from 'ol/layer/Tile';
+// import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+
+import Draw from 'ol/interaction/Draw';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 
 const Zadatak3 = () => {
-  return <div>Zadatak3</div>;
+  const drawSource = new VectorSource();
+  const drawLayer = new VectorLayer({
+    source: drawSource,
+  });
+
+  const draw = new Draw({
+    source: drawSource,
+    type: 'LineString',
+  });
+
+  useEffect(() => {
+    let map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        drawLayer,
+      ],
+      view: new View({
+        center: fromLonLat([17.19, 44.77]),
+        zoom: 4,
+      }),
+    });
+
+    map.addInteraction(draw);
+
+    draw.on('drawend', () => {
+      console.log('op');
+    });
+
+    return () => {
+      let deleteMap = document.getElementById('map');
+      if (deleteMap) {
+        deleteMap.innerHTML = '';
+      }
+    };
+  }, []);
+
+  return (
+    <div className='open-layers'>
+      <h2>OpenLayers</h2>
+      <div id='map' className='map'></div>
+    </div>
+  );
 };
 
 export default Zadatak3;
